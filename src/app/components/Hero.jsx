93e09df8f3b8b78313e5656686d6da0e45271426
azim308 <azim.gfuturetech@gmail.com>
2024-11-09 15:57:11 +0530

    Carousel Implemented and bugs and errors resolved

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -4,6 +4,11 @@ import Head from "next/head";
 import { Box, Button, Typography } from "@mui/material";
 import Image from "next/image";
 import hero1 from "../../../public/hero/hero1.svg";
+import hero2 from "../../../public/slider2.gif";
+import hero3 from "../../../public/hero3.gif";
+import hero4 from "../../../public/hero4.png";
+import Slider from "react-slick";
+import Link from "next/link";
 
 export const metadata = {
   title: "Reliable Machinery for Heavy Constructions",
@@ -12,6 +17,14 @@ export const metadata = {
 const Hero = () => {
   const styles = {
     container: {
+      position: "relative",
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    container2: {
       position: "relative",
       width: "100vw",
       height: "100vh",
@@ -41,6 +54,7 @@ const Hero = () => {
     buttonContainer: {
       width: { xs: "100%", md: "47%", lg: "36%", xl: "31%", xxl: "26%" },
       display: "flex",
+      gap: "20px",
       flexDirection: { xs: "column", md: "row" },
       justifyContent: "space-between",
       alignItems: "center",
@@ -48,7 +62,15 @@ const Hero = () => {
       mt: "40px",
     },
   };
-
+  const settings = {
+    dots: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  };
   return (
     <>
       <Head>
@@ -58,47 +80,285 @@ const Hero = () => {
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
         />
       </Head>
-      <Box sx={styles.container}>
-        <Image
-          src={hero1}
-          alt="hero1 image"
-          fill
-          style={{ objectFit: "cover" }}
-        />
-        <Box sx={{ position: "absolute", width: "80%" }}>
-          <Typography sx={styles.heading}>
-            RELIABLE MACHINERY FOR HEAVY CONSTRUCTIONS
-          </Typography>
-          <Typography sx={styles.subHeading}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsum dolor sit amet.
-          </Typography>
-          <Box sx={styles.buttonContainer}>
-            <Button
-              variant="contained"
+      <Slider {...settings}>
+        <Box sx={styles.container}>
+          <Image
+            src={hero1}
+            alt="hero1 image"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+          <Box
+            sx={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Box
               sx={{
-                color: "white",
-                height: { xl: "40px", xxl: "48px" },
-                width: "174px",
-                mb: { xs: "20px", md: 0 },
+                position: "absolute",
+                width: "80%",
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "Center",
+                flexDirection: "column",
               }}
             >
-              Explore more
-            </Button>
-            <Button
-              variant="outlined"
-              sx={{
-                color: "white",
-                height: { xl: "40px", xxl: "48px", borderWidth: "2px" },
-                width: "165px",
-              }}
-            >
-              Get in touch
-            </Button>
+              <Typography sx={styles.heading}>
+                RELIABLE MACHINERY FOR HEAVY CONSTRUCTIONS
+              </Typography>
+              <Typography sx={styles.subHeading}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet.
+              </Typography>
+              <Box sx={styles.buttonContainer}>
+                <Link href="/about-us">
+                  <Button
+                    variant="contained"
+                    sx={{
+                      color: "white",
+                      height: { xl: "40px", xxl: "48px" },
+                      width: "174px",
+                      mb: { xs: "20px", md: 0 },
+                      "&:hover": {
+                        backgroundColor: "white",
+                        color: "#00AFF9",
+                      },
+                    }}
+                  >
+                    Explore more
+                  </Button>
+                </Link>
+                <Button
+                  variant="outlined"
+                  sx={{
+                    color: "white",
+                    height: { xl: "40px", xxl: "48px", borderWidth: "2px" },
+                    width: "165px",
+                    "&:hover": {
+                      backgroundColor: "white",
+                      color: "#00AFF9",
+                    },
+                  }}
+                >
+                  Get in touch
+                </Button>
+              </Box>
+            </Box>
           </Box>
         </Box>
-      </Box>
+        <Box sx={styles.container2}>
+          <Image
+            src={hero2}
+            alt="hero1 image"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+          <Box
+            sx={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              background: "rgba(0, 40, 57, 0.7)",
+            }}
+          >
+            <Box
+              sx={{
+                position: "absolute",
+                width: "80%",
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "Center",
+                flexDirection: "column",
+              }}
+            >
+              <Typography sx={styles.heading}>
+                RELIABLE MACHINERY FOR HEAVY CONSTRUCTIONS
+              </Typography>
+              <Typography sx={styles.subHeading}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet.
+              </Typography>
+              <Box sx={styles.buttonContainer}>
+                <Link href="/about-us">
+                  <Button
+                    variant="contained"
+                    sx={{
+                      color: "white",
+                      height: { xl: "40px", xxl: "48px" },
+                      width: "174px",
+                      mb: { xs: "20px", md: 0 },
+                      "&:hover": {
+                        backgroundColor: "white",
+                        color: "#00AFF9",
+                      },
+                    }}
+                  >
+                    Explore more
+                  </Button>
+                </Link>
+                <Button
+                  variant="outlined"
+                  sx={{
+                    color: "white",
+                    height: { xl: "40px", xxl: "48px", borderWidth: "2px" },
+                    width: "165px",
+                    "&:hover": {
+                      backgroundColor: "white",
+                      color: "#00AFF9",
+                    },
+                  }}
+                >
+                  Get in touch
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        <Box sx={styles.container}>
+          <Image
+            src={hero3}
+            alt="hero1 image"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+          <Box
+            sx={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              background: "rgba(0, 40, 57, 0.8)",
+            }}
+          >
+            <Box
+              sx={{
+                position: "absolute",
+                width: "80%",
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "Center",
+                flexDirection: "column",
+              }}
+            >
+              <Typography sx={styles.heading}>
+                RELIABLE MACHINERY FOR HEAVY CONSTRUCTIONS
+              </Typography>
+              <Typography sx={styles.subHeading}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet.
+              </Typography>
+              <Box sx={styles.buttonContainer}>
+                <Link href="/about-us">
+                  <Button
+                    variant="contained"
+                    sx={{
+                      color: "white",
+                      height: { xl: "40px", xxl: "48px" },
+                      width: "174px",
+                      mb: { xs: "20px", md: 0 },
+                      "&:hover": {
+                        backgroundColor: "white",
+                        color: "#00AFF9",
+                      },
+                    }}
+                  >
+                    Explore more
+                  </Button>
+                </Link>
+                <Button
+                  variant="outlined"
+                  sx={{
+                    color: "white",
+                    height: { xl: "40px", xxl: "48px", borderWidth: "2px" },
+                    width: "165px",
+                    "&:hover": {
+                      backgroundColor: "white",
+                      color: "#00AFF9",
+                    },
+                  }}
+                >
+                  Get in touch
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        <Box sx={styles.container}>
+          <Image
+            src={hero4}
+            alt="hero1 image"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+          {/* <Box
+            sx={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              background: "rgba(0, 40, 57, 0.8)",
+            }}
+          >
+            <Box
+              sx={{
+                position: "absolute",
+                width: "80%",
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "Center",
+                flexDirection: "column",
+              }}
+            >
+              <Typography sx={styles.heading}>
+                RELIABLE MACHINERY FOR HEAVY CONSTRUCTIONS
+              </Typography>
+              <Typography sx={styles.subHeading}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet.
+              </Typography>
+              <Box sx={styles.buttonContainer}>
+                <Button
+                  variant="contained"
+                  sx={{
+                    color: "white",
+                    height: { xl: "40px", xxl: "48px" },
+                    width: "174px",
+                    mb: { xs: "20px", md: 0 },
+                  }}
+                >
+                  Explore more
+                </Button>
+                <Button
+                  variant="outlined"
+                  sx={{
+                    color: "white",
+                    height: { xl: "40px", xxl: "48px", borderWidth: "2px" },
+                    width: "165px",
+                  }}
+                >
+                  Get in touch
+                </Button>
+              </Box>
+            </Box>
+          </Box> */}
+        </Box>
+      </Slider>
     </>
   );
 };
